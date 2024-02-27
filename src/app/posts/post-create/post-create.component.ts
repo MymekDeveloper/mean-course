d5db6db94327eb05c6a66a9c89b3c321e1558765
MymekDeveloper <szwed.szymon1@gmail.com>
2024-02-27 23:28:21 +0100

    Added first form, in place of two-way binding

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Post } from '../post.model';
+
+@Component({
+  selector: 'app-post-create', // This is the HTML tag that will be used to render the component)
+  templateUrl: './post-create.component.html', // This is the HTML file that will be used to render the component)
+  styleUrls: ['./post-create.component.css'], // This is the CSS file that will be used to style the component)
+})
+export class PostCreateComponent {
+  enteredContent = '';
+  enteredTitle = '';
+  @Output() postCreated = new EventEmitter<Post>();
+
+  onAddPost(form: NgForm) {
+    const post: Post = {
+      title: form.value.title,
+      content: form.value.content,
+    };
+    this.postCreated.emit(post);
+  }
+}
