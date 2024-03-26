c527f8f385d5fd5f115fc4c3ce0b4d6d347509da
MymekDeveloper <szwed.szymon1@gmail.com>
2024-03-27 00:29:13 +0100

    Added first posts as JSON file

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First middleware");
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send("Hello from Express");
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "1",
+      title: "First post",
+      content: "First content",
+    },
+    {
+      id: "2",
+      title: "Second post",
+      content: "Second content",
+    },
+  ];
+  res.status(200).json({
+    message: "Posts fetched succesfully",
+    posts: posts,
+  });
 });
 
 module.exports = app;
