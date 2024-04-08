7214d8be3db6fcd0e0636cc05272cf3b3387265f
MymekDeveloper <szwed.szymon1@gmail.com>
2024-04-08 23:21:39 +0200

    Added edit functionality

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,24 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-
 import { PostsService } from '../posts.service';
+
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-create', // This is the HTML tag that will be used to render the component)
   templateUrl: './post-create.component.html', // This is the HTML file that will be used to render the component)
   styleUrls: ['./post-create.component.css'], // This is the CSS file that will be used to style the component)
 })
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit {
   enteredContent = '';
   enteredTitle = '';
+  post: Post;
+  private mode = 'create';
+  private postId: string;
 
-  constructor(public postsService: PostsService) {}
+  constructor(
+    public postsService: PostsService,
+    public route: ActivatedRoute
+  ) {}
 
-  onAddPost(form: NgForm) {
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
+        this.post = this.postsService.getPost(this.postId);
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
+  }
+
+  onSavePost(form: NgForm) {
     if (form.invalid) {
       return;
     }
-    this.postsService.addPost(form.value.title, form.value.content);
+    if (this.mode === 'create') {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.updatePost(
+        this.postId,
+        form.value.title,
+        form.value.content
+      );
+    }
+
     form.resetForm();
   }
 }
